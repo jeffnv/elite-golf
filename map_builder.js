3602f8e05e7f0373bbd79866027aaae06c868668
Jeff Fiddler <jeffnv@gmail.com>
2014-12-29 12:02:10 -0800

    random course

--- map_builder.js
+++ map_builder.js
@@ -1,9 +1,76 @@
 MapBuilder = {
+    randomMap: function() {
+        var ballLoc = this._randomCoords();
+        var holeLoc = this._randomCoords();
+        var wallCount = Math.random() * 5;
+        var trapCount = Math.random() * 5;
+        var walls = this._randomWalls(wallCount);
+        var traps = this._randomTraps(trapCount);
+        var par = this._rand(1, 4);
+        return this.buildMap({
+          par: par,
+          ballLoc: ballLoc,
+          holeLoc: holeLoc,
+          walls: walls,
+          traps: traps
+        });
+    },
     buildMap: function(options) {
         var map = MapBuilder.emptyMap();
-        //merge options
         MapBuilder._mergeMapOptions(map, options);
         return map;
+    },
+    _rand: function(min, max) {
+        var max = max || 500;
+        var min = min || 0;
+        var range = max - min;
+        return Math.floor(Math.random() * range + min);
+    },
+    _randomCoords: function() {
+        var rX = this._rand();
+        var rY = this._rand();
+        return {
+            x: rX,
+            y: rY
+        };
+    },
+    _randomCoordSet: function() {
+        var x1 = this._rand(20, 400);
+        var y1 = this._rand(20, 400);
+        var x2 = this._rand(x1, 480);
+        var y2 = this._rand(y1, 480);
+        return [
+            [x1, y1],
+            [x2, y2]
+        ];
+    },
+    _randomWall: function() {
+        var coordSet = this._randomCoordSet();
+        return {
+            start: { x: coordSet[0][0], y: coordSet[0][1] },
+            end: { x: coordSet[1][0], y: coordSet[1][1] }
+        };
+    },
+    _randomWalls: function(count) {
+        var walls = [];
+        for (var i = 0; i < count; i++) {
+            walls.push(this._randomWall());
+        }
+        return walls;
+    },
+    _randomTrap: function() {
+        var coordSet = this._randomCoordSet();
+        return {
+            topLeft: { x: coordSet[0][0], y: coordSet[0][1] },
+            bottomRight: { x: coordSet[1][0], y: coordSet[1][1] }
+        };
+    },
+    _randomTraps: function(count) {
+        var traps = [];
+        for (var i = 0; i < count; i++) {
+            traps.push(this._randomTrap());
+        }
+        return traps;
     },
     _mergeMapOptions: function(defaults, options) {
         for (prop in options) {
@@ -13,7 +80,7 @@ MapBuilder = {
                     defaults['walls'].push(wall);
                 });
             } else {
-              defaults[prop] = options[prop];
+                defaults[prop] = options[prop];
             }
         }
     },
--- maps.js
+++ maps.js
@@ -1,47 +1,50 @@
 MAPS = []
+for(var i = 0; i < 9; i++){
+  MAPS.push(MapBuilder.randomMap());
+}
 
 //start with par 1 empty map
-var map;
-
-map = MapBuilder.buildMap({
-    par: 3,
-    ballLoc: {x: 30, y: 30},
-    holeLoc: {x: 450, y: 450},
-    traps: [
-      { 
-        topLeft: { x: 100, y: 30 }, 
-        bottomRight: { x: 300, y: 400 } 
-      },
-    ],
-});
-
-MAPS.push(map);
-MAPS.push(MapBuilder.emptyMap());
-
-//big vertical wall
-map = MapBuilder.buildMap({
-    par: 2,
-    ballLoc: {x: 30, y: 30},
-    holeLoc: {x: 450, y: 450},
-    traps: [
-      { 
-        topLeft: { x: 100, y: 30 }, 
-        bottomRight: { x: 300, y: 400 } 
-      },
-    ],
-    walls: [
-        { start: { x: 50, y: 5 }, end: { x: 50, y: 400 }, }
-    ]
-})
-MAPS.push(map);
-
-
-//just a diagonal wall
-map = MapBuilder.buildMap({
-    par: 3,
-    walls: [
-        { start: { x: 50, y: 50 }, end: { x: 450, y: 450 }, }
-    ]
-})
-MAPS.push(map);
-
+// var map;
+//
+// map = MapBuilder.buildMap({
+//     par: 3,
+//     ballLoc: {x: 30, y: 30},
+//     holeLoc: {x: 450, y: 450},
+//     traps: [
+//       { 
+//         topLeft: { x: 100, y: 30 }, 
+//         bottomRight: { x: 300, y: 400 } 
+//       },
+//     ],
+// });
+//
+// MAPS.push(map);
+// MAPS.push(MapBuilder.emptyMap());
+//
+// //big vertical wall
+// map = MapBuilder.buildMap({
+//     par: 2,
+//     ballLoc: {x: 30, y: 30},
+//     holeLoc: {x: 450, y: 450},
+//     traps: [
+//       { 
+//         topLeft: { x: 100, y: 30 }, 
+//         bottomRight: { x: 300, y: 400 } 
+//       },
+//     ],
+//     walls: [
+//         { start: { x: 50, y: 5 }, end: { x: 50, y: 400 }, }
+//     ]
+// })
+// MAPS.push(map);
+//
+//
+// //just a diagonal wall
+// map = MapBuilder.buildMap({
+//     par: 3,
+//     walls: [
+//         { start: { x: 50, y: 50 }, end: { x: 450, y: 450 }, }
+//     ]
+// })
+// MAPS.push(map);
+//
