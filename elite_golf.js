11dda1589da6db833fb70361cf48e6b23b90a5aa
Jeff Fiddler <jeffnv@gmail.com>
2015-01-09 16:01:53 -0800

    nearly there. many architectural improvements

--- elite_golf.js
+++
@@ -1,6 +0,0 @@
-function GolfGame(canvas){
-  GameStateMachine.call(this, canvas);
-  this.modes = [WelcomeScreen, Level];
-}
-
-GolfGame.prototype = Object.create(GameStateMachine.prototype);
--- golf_map_creator.js
+++
@@ -1,6 +0,0 @@
-function GolfMapCreator(canvas){
-  GameStateMachine.call(this, canvas);
-  this.modes = [WelcomeScreen, MapCreator];
-}
-
-GolfMapCreator.prototype = Object.create(GameStateMachine.prototype);
